9fc5f55fb0daa41728d9d7d02e7693dcf79fde05
Denis Filoniuk <kazzus.06@gmail.com>
2016-11-23 16:37:04 +0200

    O-open closed principle class before changes

---
+++ SOLID_principles/Customer1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOLID_principles
+{
+    //“O” - Open closed principle
+    public class Customer1
+    {
+        private int _CustType;
+
+        public int CustType
+        {
+            get { return _CustType; }
+            set { _CustType = value; }
+        }
+
+        public double getDiscount(int TotalSales)
+        {
+            if (_CustType == 1)
+            {
+                return TotalSales - 100;
+            }
+            else
+            {
+                return TotalSales - 50;
+            }
+        }
+    }
+}
